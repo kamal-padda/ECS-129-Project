82fec03e6db40107ca2176758cb58891bab46177
AlanChuang1 <afchuang@ucdavis.edu>
2022-03-01 21:52:30 -0800

    splice function

---
+++ splice.py
@@ -0,0 +1,26 @@
+def splice(mrnaSequence):
+    final_seq = ""
+    splice_seq = ""
+    pair = []      # Will be set to False once splicing has been started, but not yet completed
+    # Will iterate through twice, for each reading Frame that could contain a splice site
+    for x in range(2):
+        splice = False
+        for i in range(x, len(mrnaSequence), 2):
+            pair = mrnaSequence[i:i + 2]
+            if pair == 'GU' and splice == False:
+                 splice_seq += pair
+                 splice = True
+            elif pair == 'AG' and splice == True:
+                 splice_seq += pair
+                 splice = False
+            else:
+                if splice == True:
+                    splice_seq += pair
+    final_seq = mrnaSequence.replace(splice_seq, '')
+    print("mRNA sequence after splicing:", final_seq)
+    return final_seq
+
+
+splice("CCGUCCCCCCCCAGCC")
+splice("CGUCCCCCCCCAGC")
+
