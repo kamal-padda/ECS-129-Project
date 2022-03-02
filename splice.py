5766158cbe2c5335bcf3644e9fe387987869efe9
AlanChuang1 <afchuang@ucdavis.edu>
2022-03-01 22:10:45 -0800

    fixed to run with odd number sequences

--- splice.py
+++ splice.py
@@ -1,12 +1,21 @@
 def splice(mrnaSequence):
+    frames = []
+    
+    for x in range(2):
+        frame = ""
+        for i in range(x, len(mrnaSequence), 2):
+            if (i + 1) < len(mrnaSequence):
+                frame += mrnaSequence[i:i + 2]
+        frames.append(frame)
+
     final_seq = ""
     splice_seq = ""
     pair = ""
     # Will iterate through twice, for each reading Frame that could contain a splice site
-    for x in range(2):
+    for frame in frames:
         splice = False
-        for i in range(x, len(mrnaSequence), 2):
-            pair = mrnaSequence[i:i + 2]
+        for i in range(0, len(frame), 2):
+            pair = frame[i:i + 2]
             if pair == 'GU' and splice == False:
                  splice_seq += pair
                  splice = True
@@ -15,12 +24,14 @@ def splice(mrnaSequence):
                  splice = False
             else:
                 if splice == True:
-                    splice_seq += pair
+                    splice_seq += pair 
+                    print("append", pair)
+    print(splice_seq)
     final_seq = mrnaSequence.replace(splice_seq, '')
     print("mRNA sequence after splicing:", final_seq)
     return final_seq
 
 
 splice("CCGUCCCCCCCCAGCC")
-splice("CGUCCCCCCCCAGC")
+splice("CUGUCCCcCCCCAGCU")
 
